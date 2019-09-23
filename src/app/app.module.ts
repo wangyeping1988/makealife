f3fd2ae4c302ee040ce8ac9ef56a93c006ff4633
Wang Ye Ping <wangyeping1988@outlook.com>
2019-09-23 22:10:43 +0800

    1. add simple routing 2. change favicon.ico 3. add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    DashboardComponent,
     AdminComponent
   ],
   imports: [
